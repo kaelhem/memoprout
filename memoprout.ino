4239af13465143abb8dfee3904c5c8506a6bfc00
Klem <kaelhem@gmail.com>
2019-11-13 11:02:15 +0100

    use external tool to generates random games abd load them from sd card

--- memoprout.ino
+++ memoprout.ino
@@ -3,7 +3,6 @@
 #include "MemoProut_speaker.h"
 #include "MemoProut_pins.h"
 #include "MemoProut_config.h"
-#include <TrueRandom.h>
 #include <avr/pgmspace.h>
 
 #define NO_PRESSED_BUTTON 255
@@ -26,33 +25,37 @@ const char *BASIC = "BASIC";
 const char *KIDS = "KIDS";
 const char *CINEMA = "CINEMA";
 const char *gameBufferFile = "GAME.PRT";
-const char *scoreBufferFile = "CFG.PRT";
+const char *scoreBufferFile = "SCORE.PRT";
+const char *configBufferFile = "CFG.PRT";
 
 byte mainState = STARTUP;
 byte gameState = PLAY_SOUND;
 char *gameKind = BASIC;
+byte globalGameIndex = 0;
 
 MemoProut_speaker speaker = MemoProut_speaker();
 MemoProut_controller controller = MemoProut_controller();
 
+byte checkLedIndex = 0;
+
 void checkLeds() {
-  byte checkLedIndex = 0;
-  while (mainState == CHECK_LEDS) {
-    controller.listenButtons();
-    if (controller.currentPressedButton != NO_PRESSED_BUTTON) {
+  if (DEBUG_MODE) {
+    Serial.println("check leds");
+  }
+  controller.listenButtons();
+  if (controller.currentPressedButton != NO_PRESSED_BUTTON) {
+    checkLedIndex = 0;
+    gotoState(MENU);
+  } else {
+    controller.lightUp(controller.getButtonIdAtIndex(checkLedIndex));
+    delay(200);
+    if (++checkLedIndex == 28) {
       checkLedIndex = 0;
-      gotoState(MENU);
-    } else {
-      controller.lightUp(controller.getButtonIdAtIndex(checkLedIndex));
+      controller.lightUp(controller.LED_OK);
       delay(200);
-      if (++checkLedIndex == 28) {
-        checkLedIndex = 0;
-        controller.lightUp(controller.LED_OK);
-        delay(200);
-        controller.lightUp(controller.LED_KO);
-        delay(200);
-      }
-    } 
+      controller.lightUp(controller.LED_KO);
+      delay(200);
+    }
   }
 }
 
@@ -62,12 +65,46 @@ void setup() {
   controller.init();
 }
 
+void loadConfig() {
+  if (SD.exists(configBufferFile)) {
+    File configFile = SD.open(configBufferFile, FILE_READ);
+    globalGameIndex = configFile.read();
+    configFile.close();
+  }
+  // TO REMOVE !!
+  globalGameIndex = 129;
+}
+
+void updateGlobalGameIndex() {
+  ++globalGameIndex;
+  if (SD.exists(configBufferFile)) {
+    SD.remove(configBufferFile);
+  }
+  File configFile = SD.open(configBufferFile, FILE_WRITE);
+  configFile.write(globalGameIndex);
+  configFile.close();
+}
+
 void startup() {
-  controller.resetLeds();
-  gotoState(MENU);
+  if (!speaker.isReady()) {
+    controller.blinkLed(controller.LED_KO, 3, 100);
+    controller.showMessage(F("NO SD"), 150);
+  } else {
+    loadConfig();
+    controller.blinkLed(controller.LED_OK, 1, 100);
+    speaker.playSound(F("PROUTS/P1.WAV"));
+    controller.showMessage(F("PROUT"), 120);
+    speaker.stopSound();
+    controller.resetLeds();
+    mainState = MENU;
+    speaker.playSound(F("UI/BASIC.WAV"));
+  }
 }
 
 void gotoState(byte state) {
+  if (DEBUG_MODE) {
+    Serial.println("will go on state " + String(state));
+  }
   mainState = WAIT;
   while (controller.currentPressedButton != NO_PRESSED_BUTTON) {
     controller.listenButtons();
@@ -78,103 +115,91 @@ void gotoState(byte state) {
   mainState = state;
 }
 
+void switchGameKind() {
+  if (gameKind == BASIC) {
+    gameKind = KIDS;
+  } else if (gameKind == KIDS) {
+    gameKind = CINEMA;
+  } else {
+    gameKind = BASIC;
+  }
+  speaker.playSoundAndWait("UI/" + String(gameKind) + F(".WAV"));
+}
+
 void menu() {
   controller.listenButtons();
   switch (controller.currentPressedButton) {
-    case 0: 
+    case BT_1: 
       // basic game
-      gameState = PREPARE_GAME;
-      gameKind = BASIC;
-      gotoState(GAME_LOOP);
+      switchGameKind();
     break;
-    case 1:
-      // kids game
-      gameState = PREPARE_GAME;
-      gameKind = KIDS;
-      gotoState(GAME_LOOP);
-    break;
-    case 15:
-      // movie game
-      gameState = PREPARE_GAME;
-      gameKind = CINEMA;
-      gotoState(GAME_LOOP);
-    break;
-    case 27:
+    case BT_22:
       if (!speaker.isPlaying()) {
-        File scoreFile = SD.open(scoreBufferFile, FILE_READ);
-        byte scoreSoundIndex = max(0, min(scoreFile.read(), 10));
-        scoreFile.close();
+        byte scoreSoundIndex = max(0, min(readHiScore(), 10));
         if (scoreSoundIndex > 0) {
           speaker.playSoundAndWait(F("UI/HISCORE.WAV"));
         }
-        speaker.playSoundAndWait("SCORE/S" + String(scoreSoundIndex) + ".WAV");
+        speaker.playSoundAndWait("SCORE/S" + String(scoreSoundIndex) + F(".WAV"));
       }
     break;
-    case 12:
+    case BT_7:
       // vol +
       if (speaker.canUpVolume && !speaker.isPlaying()) {
         speaker.upVolume();
         speaker.playSound(F("PROUTS/P10.WAV"));
       }
     break;
-    case 13:
+    case BT_14:
       // vol -
       if (speaker.canDownVolume && !speaker.isPlaying()) {
         speaker.downVolume();
         speaker.playSound(F("PROUTS/P10.WAV"));
       }
     break;
-    case 26:
-      // check leds
-      gotoState(CHECK_LEDS);
+    case BT_28:
+      //gotoState(CHECK_LEDS);
+      gameState = PREPARE_GAME;
+      gotoState(GAME_LOOP);
     break;
   }
-  byte ledsToEnlight[] = { 0, 1, 15, 26, 27 };
+  byte ledsToEnlight[] = { BT_1, BT_22, BT_7, BT_14, BT_28 };
   const byte numLeds = sizeof(ledsToEnlight) / sizeof(byte);
   controller.multiLightUp(ledsToEnlight, numLeds);
 }
 
 byte waitButtonRelease = NO_PRESSED_BUTTON;
-byte firstWaitButton;
-byte currentWaitButton;
+GameSound firstWaitSound;
+GameSound *currentWaitSound;
 byte currentWaitButtonIndex;
 byte score;
 
 void prepareNewGame() {
   controller.resetLeds();
   score = 0;
-  byte kind = 0;
   currentWaitButtonIndex = 0;
+
+  byte i;
+
+  // read the next game ref (pre-registered random values)
+  byte randomGame[84];
+  File refGameFile = SD.open("GAMES/" + String(gameKind) + F(".PRT"), FILE_READ);
+  refGameFile.seek(84 * globalGameIndex);
+  for (i = 0; i < 84; ++i) {
+    randomGame[i] = refGameFile.read();
+  }
+  refGameFile.close();
+
+  // write this game in buffer file
   if (SD.exists(gameBufferFile)) {
     SD.remove(gameBufferFile);
   }
   File gameFile = SD.open(gameBufferFile, FILE_WRITE);
-  byte randomButton;
-  byte soundVariant;
-  for (byte i = 0; i < 30; ++i) {
-    randomButton = TrueRandom.random(0, 28);
-    if (gameKind == BASIC) {
-      soundVariant = TrueRandom.random(1, kind == 3 ? 5 : 9);
-    } else if (gameKind == CINEMA) {
-      soundVariant = TrueRandom.random(1, 15);
-    } else  {
-      soundVariant = TrueRandom.random(1, 5);
-    }
-    if (DEBUG_MODE) {
-      Serial.println("write " + String(i) + ": " + String(kind) + ", " + String(soundVariant) + ", " + String(randomButton));
-    }
-    gameFile.write(kind);
-    gameFile.write(soundVariant);
-    gameFile.write(randomButton);
-    if (i == 0) {
-      firstWaitButton = randomButton;
-    }
-    ++kind;
-    if ((kind == 4 && gameKind == BASIC) || (kind == 3 && gameKind != BASIC)) {
-      kind = 0;
-    }
+  for (i = 0; i < 84; ++i) {
+    gameFile.write(randomGame[i]);
   }
   gameFile.close();
+  firstWaitSound = {randomGame[0], randomGame[1], randomGame[2]};
+  updateGlobalGameIndex();
   gameState = PLAY_SOUND;
 }
 
@@ -222,7 +247,7 @@ void gameLoop() {
         GameSound gameSound = getGameSoundAt(score);
         controller.lightUp(gameSound.buttonId);
         currentWaitButtonIndex = 0;
-        currentWaitButton = firstWaitButton;
+        currentWaitSound = &firstWaitSound;
         waitButtonRelease = NO_PRESSED_BUTTON;
         speaker.playSoundAndWait(getFilename(gameSound.kind, gameSound.variant));
         controller.resetLeds();
@@ -236,7 +261,7 @@ void gameLoop() {
         speaker.playSoundAndWait(getFilename(gameSound.kind, gameSound.variant));
         controller.resetLeds();
         currentWaitButtonIndex = 0;
-        currentWaitButton = firstWaitButton;
+        currentWaitSound = &firstWaitSound;
         waitButtonRelease = NO_PRESSED_BUTTON;
         gameState = WAIT_BUTTON;
       }
@@ -260,23 +285,19 @@ void gameLoop() {
           waitButtonRelease = controller.currentPressedButton;
         }
       } else if (waitButtonRelease != NO_PRESSED_BUTTON) {
-        if (waitButtonRelease == currentWaitButton) {
-          byte ledsToEnlight[] = { controller.LED_OK, currentWaitButton };
+        if (waitButtonRelease == currentWaitSound->buttonId) {
+          byte ledsToEnlight[] = { controller.LED_OK, currentWaitSound->buttonId };
           for (byte lightTimer = 0; lightTimer < 100; ++lightTimer) {
             controller.multiLightUp(ledsToEnlight, 2); 
           }
-          /*
-          controller.lightUp(currentWaitButton);
-          GameSound currentGameSound = getGameSoundAt(currentWaitButtonIndex);
-          speaker.playSound(getFilename(currentGameSound.kind, currentGameSound.variant));
+          controller.lightUp(currentWaitSound->buttonId);
+          speaker.playSound(getFilename(currentWaitSound->kind, currentWaitSound->variant));
           while(speaker.isPlaying()) {
             delay(1);
           }
-          */
           controller.resetLeds();
           if (++currentWaitButtonIndex <= score) {
-            GameSound gameSound = getGameSoundAt(currentWaitButtonIndex);
-            currentWaitButton = gameSound.buttonId;
+            currentWaitSound = &getGameSoundAt(currentWaitButtonIndex);
           } else {
             score += 1;
             gameState = PLAY_SOUND;
@@ -290,50 +311,35 @@ void gameLoop() {
   }  
 }
 
+void gameOver() {
+  byte hiScore = readHiScore();
+  if (score > hiScore) {
+    writeHiScore(score);
+  }
+  controller.blinkLed(controller.LED_KO, 3, 100);
+  speaker.playSoundAndWait(F("UI/LOOSE.WAV"));
+  if (score > 0) {
+    speaker.playSoundAndWait(F("UI/URSCORE.WAV"));
+    byte scoreSoundIndex = max(0, min(score, 10));
+    speaker.playSoundAndWait("SCORE/S" + String(scoreSoundIndex) + F(".WAV"));
+  }
+  if (score < 3) {
+    speaker.playSound(String(gameKind) + F("/LOOSE.WAV"));
+  }
+  controller.showMessage(F("YOU LOOSE"), 160);
+  while (speaker.isPlaying()) {
+    delay(1);
+  }
+  gotoState(MENU);
+}
+
 void loop() {
   switch(mainState) {
-    case STARTUP:
-      if (!speaker.isReady()) {
-        controller.blinkLed(controller.LED_KO, 3, 100);
-        controller.showMessage(F("NO SD"), 150);
-      } else {
-        controller.blinkLed(controller.LED_OK, 1, 100);
-        speaker.playSound(F("PROUTS/P1.WAV"));
-        controller.showMessage(F("PROUT"), 120);
-        speaker.stopSound();
-        startup();
-      }
-    break;
-    case MENU:
-      menu();
-    break;
-    case GAME_LOOP:
-      gameLoop();
-    break;
-    case GAME_OVER:
-      byte hiScore = readHiScore();
-      if (score > hiScore) {
-        writeHiScore(hiScore);
-      }
-      controller.blinkLed(controller.LED_KO, 3, 100);
-      speaker.playSoundAndWait(F("UI/LOOSE.WAV"));
-      if (score > 0) {
-        speaker.playSoundAndWait(F("UI/URSCORE.WAV"));
-        byte scoreSoundIndex = max(0, min(score, 10));
-        speaker.playSoundAndWait("SCORE/S" + String(scoreSoundIndex) + ".WAV");
-      }
-      if (score < 3) {
-        speaker.playSound(String(gameKind) + F("/LOOSE.WAV"));
-      }
-      controller.showMessage(F("YOU LOOSE"), 160);
-      while (speaker.isPlaying()) {
-        delay(1);
-      }
-      gotoState(MENU);
-    break;
-    case CHECK_LEDS:
-      checkLeds();
-    break;
+    case STARTUP: startup(); break;
+    case MENU: menu(); break;
+    case GAME_LOOP: gameLoop(); break;
+    case GAME_OVER: gameOver(); break;
+    case CHECK_LEDS: checkLeds(); break;
   }
 }
 
