9902b98febd7972ab27e4c8ef97d6ae09417d90b
Klem <kaelhem@gmail.com>
2019-12-07 14:35:26 +0100

    fix icon color in footer

--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import logo from 'icons/logo-filled.svg'
+import whiteLogo from 'icons/logo-filled.svg'
+import greyLogo from 'icons/logo-filled-grey.svg'
 
-const Footer = ({bgColor, color}) => (
-  <div style={{ width: '100vw', height: 20, paddingTop: 1, textAlign: 'center', fontSize: 11, color: color || '#999', background: bgColor || '#fff' }}>
-    <div>kaelhem © 2019 - Made with <img src={ logo } width="10" alt="logo" /> in Bouguenais-les-Couëts</div>
-  </div>
-)
+const Footer = ({bgColor, color}) => {
+  const img = color ? whiteLogo : greyLogo
+  return (
+    <div style={{ width: '100vw', height: 20, paddingTop: 1, textAlign: 'center', fontSize: 11, color: color || '#999', background: bgColor || '#fff' }}>
+      <div>kaelhem © 2019 - Made with <img src={ img } width="10" alt="logo" /> in Bouguenais-les-Couëts</div>
+    </div>
+  )
+}
 
 export default Footer
