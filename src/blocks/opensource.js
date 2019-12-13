bbda266c0b4d3e57ed7fb95bda5e070d94f77314
Klem <kaelhem@gmail.com>
2019-12-13 13:39:55 +0100

    wording update

--- src/blocks/opensource.js
+++ src/blocks/opensource.js
@@ -18,7 +18,7 @@ const Opensource = () => (
         <img src={iconCncMill} alt="CNC Router by Nick Green from the Noun Project" />
       </div>
     </div>
-    <p>L'intégralité des sources (schèma électronique, plans de découpe cnc/laser, fichiers d'impression 3D, code source et données) sont disponibles sur le <a href="https://github.com/kaelhem/memoprout">dépôt github du projet</a></p>
+    <p>L'intégralité des sources (schèma électronique, fichiers d'impression 3D, code source et données) sont disponibles sur le <a href="https://github.com/kaelhem/memoprout">dépôt github du projet</a></p>
   </Fragment>
 )
 
